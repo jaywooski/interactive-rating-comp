0e799256bc36fd73c30a0621c4438f922ad877f6
Woogster <john.m.wooley@gmail.com>
2022-05-04 01:17:10 -0500

    Finished.. added sessionStorage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const submitBtn = document.getElementById("submitBtn"); 
 const main = document.getElementById("main");
+const rating = document.querySelector(".ratings")
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
 const three = document.querySelector("#three");
@@ -7,6 +8,7 @@ const four = document.querySelector("#four");
 const five = document.querySelector("#five");
 
 function renderThankYou() {
+    var rate = sessionStorage.getItem('myRate');
     main.innerHTML = "";
     main.innerHTML = 
     `<section class="response">
@@ -14,7 +16,7 @@ function renderThankYou() {
             <img src="./images/illustration-thank-you.svg" alt="Thank You :)">
         </div>   
         <div class="response">
-            <p id="selection">You selected <!-- Add rating here --> out of 5</p>
+            <p id="selection">You selected ${rate} out of 5</p>
             <h1>Thank you!</h1>
             <p>We appreciate you taking the time to give a rating. If you ever need more support, 
                 don’t hesitate to get in touch!</p>
@@ -23,46 +25,22 @@ function renderThankYou() {
     
 }
 
-function ratingHandler(rating) {
-    rating = [ one, two, three, four, five ];
+function ratingHandler(e) {
     
-    var childs = one.innerHTML;
-    console.log(childs);
-
-    for (let i = 0; i < rating.length; i++) {
-        
-        switch (rating[i]){ 
-            case one:
-                console.log("1")
-                break;
-            case two:
-                return "2"
-                break;
-            case three:
-                return "3"
-                break;
-            case four:
-                return "4"
-                break;
-            case five:
-                return "5"
-                break;
-            default:
-                console.log("not detecting child elements")
-                break;
-        }
+    // console.log(e.target);
+    if(e.target != submitBtn) {
+        const rating = e.target.innerHTML;
+        console.log(rating);
+        sessionStorage.setItem('myRate', JSON.stringify(rating));
+        return rating;
         
     }
-
+    
     
 }
 
-// ratingHandler();
 
-one.addEventListener('click', ratingHandler)
-two.addEventListener('click', ratingHandler)
-three.addEventListener('click', ratingHandler)
-four.addEventListener('click', ratingHandler)
-five.addEventListener('click', ratingHandler)
+
+rating.addEventListener('click', ratingHandler);
 
 submitBtn.addEventListener('click', renderThankYou)
